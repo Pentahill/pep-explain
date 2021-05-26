62478e2192e02179f67f16ea4138e2129cf1ae25
刘金尧 <liujinyao@CN1514000607M.local>
2021-05-26 13:52:32 +0800

    参数换行缩进，左括号后有参数，则换行后参数与其垂直对齐。

--- indentation.py
+++ indentation.py
@@ -4,6 +4,7 @@
 # 每行的最大长度为79字符
 # Add 4 spaces (an extra level of indentation) to distinguish arguments from
 # the rest.
+# 参数换行缩进，避免参数与函数体缩进一致，参数可再缩进一层。
 def long_function_name(
         var_one, var_two, var_three,
         var_four):
@@ -17,7 +18,9 @@ foo = long_function_name(var_one, var_two,
                          var_three, var_four)
 
 # Hanging indents should add a level.
-foo = long_function_name(var_one, var_two,
-                         var_three, var_four)
+# 参数换行，缩进4空格
+foo = long_function_name(
+    var_one, var_two,
+    var_three, var_four)
 
 # 文件最后一行为空行
