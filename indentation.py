4c73966f438d685d3f979a246221e0621e207c57
刘金尧 <liujinyao@CN1514000607M.local>
2021-05-26 13:45:30 +0800

    文件最后一行为空行

---
+++ indentation.py
@@ -0,0 +1,23 @@
+# Correct:
+
+# 类或者函数后空两格
+# 每行的最大长度为79字符
+# Add 4 spaces (an extra level of indentation) to distinguish arguments from
+# the rest.
+def long_function_name(
+        var_one, var_two, var_three,
+        var_four):
+    print(var_one)
+
+
+# Aligned with opening delimiter.
+var_one, var_two, var_three, var_four = 1, 2, 3, 4
+foo = long_function_name(var_one, var_two,
+                         var_three, var_four)
+
+# Hanging indents should add a level.
+foo = long_function_name(
+    var_one, var_two,
+    var_three, var_four)
+
+# 文件最后一行为空行
